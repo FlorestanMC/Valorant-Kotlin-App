47f4dec54d6c2603ccdfc603e88329408c709eeb
Florestan Melle <Florestan.melle-cappellin@etud.univ-jfc.fr>
2025-11-14 12:31:33 +0100

    début de projet clean après ajout ktor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,9 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation(libs.ktor.core)
+    implementation(libs.ktor.engine)
+    implementation(libs.ktor.log)
+    implementation(libs.ktor.content)
+    implementation(libs.ktor.serialization)
 }
